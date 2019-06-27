88e41ac5411529d18f19fa6407992dda4d0c7549
Rick Barber <barber5@illinois.edu>
2019-06-27 16:49:31 -0500

    modify demo to go to a site with ads

--- demo.py
+++ demo.py
@@ -7,10 +7,10 @@ from six.moves import range
 from automation import CommandSequence, TaskManager
 
 # The list of sites that we wish to crawl
-NUM_BROWSERS = 3
-sites = ['http://www.example.com',
-         'http://www.princeton.edu',
-         'http://citp.princeton.edu/']
+NUM_BROWSERS = 1
+sites = ['http://www.vox.com',
+         'http://www.townhall.com',
+         'http://nytimes.com']
 
 # Loads the manager preference and 3 copies of the default browser dictionaries
 manager_params, browser_params = TaskManager.load_default_params(NUM_BROWSERS)
@@ -18,21 +18,15 @@ manager_params, browser_params = TaskManager.load_default_params(NUM_BROWSERS)
 # Update browser configuration (use this for per-browser settings)
 for i in range(NUM_BROWSERS):
     # Record HTTP Requests and Responses
-    browser_params[i]['http_instrument'] = True
-    # Record cookie changes
-    browser_params[i]['cookie_instrument'] = True
-    # Record Navigations
-    browser_params[i]['navigation_instrument'] = True
-    # Record JS Web API calls
-    browser_params[i]['js_instrument'] = True
+    browser_params[i]['save_all_content'] = True
     # Enable flash for all three browsers
     browser_params[i]['disable_flash'] = False
 if platform != 'darwin':
     browser_params[0]['headless'] = True  # Launch only browser 0 headless
 
 # Update TaskManager configuration (use this for crawl-wide settings)
-manager_params['data_directory'] = '~/Desktop/'
-manager_params['log_directory'] = '~/Desktop/'
+manager_params['data_directory'] = './data/'
+manager_params['log_directory'] = './data/'
 
 # Instantiates the measurement platform
 # Commands time out by default after 60 seconds
